59cbd7fc7f69616ef9af7d34c740f2b8baa84608
Ganesh Tiwari <gtiwari333@gmail.com>
2020-09-27 19:04:09 -0500

    wro4j - configured

--- src/main/java/gt/app/config/security/SecurityConfig.java
+++ src/main/java/gt/app/config/security/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         "/static/**",
         "/swagger-ui/**",
         "/swagger-ui.html/**",
+        "/wro4j/**",
         "/" //landing page is allowed for all
     };
 
---
+++ src/main/resources/static/js/app.js
@@ -0,0 +1,32 @@
+(function () {
+        //adding spaces to test minify
+
+
+    jQuery(document).ready(function () {
+
+        var abc = "DFDF";
+
+
+
+
+        console.log(abc);
+
+
+
+
+
+
+
+
+
+
+
+        console.log(abc);
+
+
+        console.log(abc);
+
+
+    });
+
+})();
