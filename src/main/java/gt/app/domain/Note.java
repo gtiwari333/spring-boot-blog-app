efb04c6576e39fda0dd9f0124f47b3e78f93955f
Ganesh Tiwari <gtiwari333@gmail.com>
2022-07-16 12:49:38 -0500

    using batch fetch - just in case we don't have dedicated join-fetch

--- src/main/java/gt/app/domain/Note.java
+++ src/main/java/gt/app/domain/Note.java
@@ -2,6 +2,7 @@ package gt.app.domain;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.BatchSize;
 import org.hibernate.annotations.Fetch;
 import org.hibernate.annotations.FetchMode;
 
@@ -21,6 +22,7 @@ public class Note extends BaseAuditingEntity {
 
     @OneToMany(cascade = CascadeType.PERSIST, fetch = FetchType.LAZY)
     @Fetch(FetchMode.SUBSELECT)
+    @BatchSize(size = 5)
     private List<ReceivedFile> attachedFiles = new ArrayList<>();
 
 }
