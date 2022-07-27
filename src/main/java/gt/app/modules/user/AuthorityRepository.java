92472c12e20afbe2dc760986483d3681aa85959f
Ganesh Tiwari <gtiwari333@gmail.com>
2022-07-27 09:52:41 -0500

    commit id plugin, actuator and read only transactional to speed things up

--- src/main/java/gt/app/modules/user/AuthorityRepository.java
+++ src/main/java/gt/app/modules/user/AuthorityRepository.java
@@ -2,11 +2,12 @@ package gt.app.modules.user;
 
 import gt.app.domain.Authority;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 import java.util.Set;
 
 interface AuthorityRepository extends JpaRepository<Authority, String> {
-
+    @Transactional(readOnly = true)
     Set<Authority> findByNameIn(Collection<String> name);
 }
--- src/main/java/gt/app/modules/user/UserRepository.java
+++ src/main/java/gt/app/modules/user/UserRepository.java
@@ -3,14 +3,17 @@ package gt.app.modules.user;
 import gt.app.domain.AppUser;
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 interface UserRepository extends JpaRepository<AppUser, Long> {
 
     @EntityGraph(attributePaths = {"authorities"})
+    @Transactional(readOnly = true)
     Optional<AppUser> findOneWithAuthoritiesByUniqueId(String uniqueId);
 
+    @Transactional(readOnly = true)
     boolean existsByUniqueId(String uniqueId);
 
 }
