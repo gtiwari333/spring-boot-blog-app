e8a8f6d851f563c87d474c0ea5d2b999efaf5403
Ganesh Tiwari <gtiwari333@gmail.com>
2022-07-31 14:20:52 -0500

    commit id plugin, actuator and read only transactional to speed things up

--- src/main/java/gt/app/modules/user/LiteUserRepository.java
+++ src/main/java/gt/app/modules/user/LiteUserRepository.java
@@ -2,12 +2,14 @@ package gt.app.modules.user;
 
 import gt.app.domain.LiteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 interface LiteUserRepository extends JpaRepository<LiteUser, Long> {
-
+    @Transactional(readOnly = true)
     Optional<LiteUser> findOneByUniqueId(String uniqueId);
 
+    @Transactional(readOnly = true)
     Optional<LiteUser> findByIdAndActiveIsTrue(Long id);
 }
