b8e82123be3e36d3414785a75965833a7571e0ff
Ganesh Tiwari <gtiwari333@gmail.com>
2023-09-23 18:22:32 -0500

    spring boot 3.1 migration - taking care of deprecated methods + using rewrite-maven-plugin

--- src/main/java/gt/app/config/security/SecurityConfig.java
+++ src/main/java/gt/app/config/security/SecurityConfig.java
@@ -4,6 +4,7 @@ import gt.app.config.Constants;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -11,16 +12,23 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
+
+import java.util.stream.Stream;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @EnableWebSecurity
 @Configuration
 @RequiredArgsConstructor
+@EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 public class SecurityConfig {
 
     private static final String[] AUTH_WHITELIST = {
         "/swagger-resources/**",
         "/v3/api-docs/**",
-        "/h2-console/**",
         "/webjars/**",
         "/static/**",
         "/error/**",
@@ -30,13 +38,17 @@ public class SecurityConfig {
         "/" //landing page is allowed for all
     };
 
+
     @Bean
-    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    protected SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
+        var mvcH2Console = new MvcRequestMatcher.Builder(introspector).servletPath("/h2-console");
         http.headers(h -> h.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
-            .authorizeHttpRequests(ah -> ah.requestMatchers(AUTH_WHITELIST).permitAll()
-                .requestMatchers("/admin/**").hasAuthority(Constants.ROLE_ADMIN)
-                .requestMatchers("/user/**").hasAuthority(Constants.ROLE_USER)
-                .requestMatchers("/api/**").authenticated()//individual api will be secured differently
+            .authorizeHttpRequests(ah -> ah
+                .requestMatchers(Stream.of(AUTH_WHITELIST).map(AntPathRequestMatcher::antMatcher).toList().toArray(new AntPathRequestMatcher[0])).permitAll()
+                .requestMatchers(mvcH2Console.pattern("/**")).permitAll()
+                .requestMatchers(antMatcher("/admin/**")).hasAuthority(Constants.ROLE_ADMIN)
+                .requestMatchers(antMatcher("/user/**")).hasAuthority(Constants.ROLE_USER)
+                .requestMatchers(antMatcher("/api/**")).authenticated()//individual api will be secured differently
                 .anyRequest().authenticated())
             .csrf(AbstractHttpConfigurer::disable)
             .formLogin(f -> f.loginProcessingUrl("/auth/login")
