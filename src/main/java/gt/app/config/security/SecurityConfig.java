1ac0c46d4a622bc4316206e2793b2add7596223f
Ganesh Tiwari <gtiwari333@gmail.com>
2025-08-16 16:53:10 -0500

    upgrade to sb 3.5

--- src/main/java/gt/app/config/security/SecurityConfig.java
+++ src/main/java/gt/app/config/security/SecurityConfig.java
@@ -12,13 +12,6 @@ import org.springframework.security.config.annotation.web.configurers.HeadersCon
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
-
-import java.util.stream.Stream;
-
-import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @EnableWebSecurity
 @Configuration
@@ -35,20 +28,18 @@ public class SecurityConfig {
         "/swagger-ui/**",
         "/swagger-ui.html/**",
         "/signup/**",
+        "/h2-console/**",
         "/" //landing page is allowed for all
     };
 
-
     @Bean
-    protected SecurityFilterChain filterChain(HttpSecurity http, HandlerMappingIntrospector introspector) throws Exception {
-        var mvcH2Console = new MvcRequestMatcher.Builder(introspector).servletPath("/h2-console");
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.headers(h -> h.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin))
             .authorizeHttpRequests(ah -> ah
-                .requestMatchers(Stream.of(AUTH_WHITELIST).map(AntPathRequestMatcher::antMatcher).toList().toArray(new AntPathRequestMatcher[0])).permitAll()
-                .requestMatchers(mvcH2Console.pattern("/**")).permitAll()
-                .requestMatchers(antMatcher("/admin/**")).hasAuthority(Constants.ROLE_ADMIN)
-                .requestMatchers(antMatcher("/user/**")).hasAuthority(Constants.ROLE_USER)
-                .requestMatchers(antMatcher("/api/**")).authenticated()//individual api will be secured differently
+                .requestMatchers(AUTH_WHITELIST).permitAll()
+                .requestMatchers("/admin/**").hasAuthority(Constants.ROLE_ADMIN)
+                .requestMatchers( "/user/**").hasAuthority(Constants.ROLE_USER)
+                .requestMatchers( "/api/**").authenticated()//individual api will be secured differently
                 .anyRequest().authenticated())
             .csrf(AbstractHttpConfigurer::disable)
             .formLogin(f -> f.loginProcessingUrl("/auth/login")
