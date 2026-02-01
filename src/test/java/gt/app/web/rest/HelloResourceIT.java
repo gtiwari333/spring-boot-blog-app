7070d17332be42217ab610429822b0e2b766d59b
ganesh <gtiwari333@gmail.com>
2026-02-01 14:44:40 -0600

    spring boot 3.5

--- src/test/java/gt/app/web/rest/HelloResourceIT.java
+++ src/test/java/gt/app/web/rest/HelloResourceIT.java
@@ -4,7 +4,7 @@ import gt.app.config.Constants;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
