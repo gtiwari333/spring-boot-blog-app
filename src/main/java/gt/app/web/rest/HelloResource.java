2a1a38afeaf875f79791afcc8f623400a4646002
gt <gtiwari333@gmail.com>
2026-07-03 22:33:06 -0500

    cleanups

--- src/main/java/gt/app/web/rest/HelloResource.java
+++ src/main/java/gt/app/web/rest/HelloResource.java
@@ -3,32 +3,38 @@ package gt.app.web.rest;
 import gt.app.config.Constants;
 import gt.app.modules.email.dto.EmailDto;
 import gt.app.modules.email.EmailService;
+import gt.app.modules.note.NoteService;
+import gt.app.modules.note.dto.NoteReadDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
+
 import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 @Slf4j
 @Profile({Constants.SPRING_PROFILE_DEVELOPMENT, Constants.SPRING_PROFILE_TEST})
+@RequestMapping("/debug")
 public class HelloResource {
     private final EmailService emailService;
+    private final NoteService noteService;
 
-    @GetMapping("/debug/hello")
+    @GetMapping("/hello")
     public Map<String, String> sayHello() {
         return Map.of("hello", "world");
     }
 
-    @PostMapping("/debug/sendEmail")
+    @PostMapping("/sendEmail")
     public ResponseEntity<Void> sendEmailWithAttachments(@RequestBody @Valid @NotNull EmailDto email) {
         log.debug("Sending email ...");
 
@@ -36,4 +42,10 @@ public class HelloResource {
 
         return ResponseEntity.ok().build();
     }
+
+    @GetMapping("/notes")
+    public Page<NoteReadDto> index(@PageableDefault(size = 10, sort = "createdDate", direction = Sort.Direction.DESC) Pageable pageable) {
+        return noteService.readAll(pageable);
+    }
+
 }
